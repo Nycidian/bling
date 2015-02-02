35b3acd4b54bb6852dca62f260e6a449525f3a7e
Nycidian <robert.bruce.goodman@gmail.com>
2015-02-02 04:51:01 -0500

    Added equality tests
    Added tests for Loop

--- test/test.py
+++ test/test.py
@@ -1,19 +1,82 @@
 __author__ = 'Nycidian'
 
 import unittest
-import math
 
-from ring import Ring
+from ring import Ring, Loop, Setting, Band
 
 
 def alpha():
     pass
 
 
-class TestRingBase(unittest.TestCase):
+class TestLoop(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_get(self):
+        loop = Loop(0, 'hello', alpha)
+        self.assertEqual(loop[1], 'hello')
+        self.assertEqual(loop[0], 0)
+        self.assertEqual(loop[2], alpha)
+        with self.assertRaises(IndexError):
+            loop[3]
+
+    def test_set(self):
+
+        def func():
+            a = 2
+
+        this = 'hell no'
+        loop = Loop(func, 0, 12345, 'about', this, 'hell no')
+        loop2 = Loop(0, 12345, 'about', this, 'hell no', func)
+        loop3 = Loop(12345, 0, 'about', this, 'hell no', func)
+        loop4 = Loop(func, 0, 12345, 'about', this, 'hell no')
+
+        s = set([loop, loop2, loop3, loop4])
+        self.assertEqual(len(s), 3)
+
+    def test_isinstance(self):
+
+        def func():
+            a = 2
+
+        one = Loop(1, 3, 2)
+        two = Loop('1', 3, func)
+        three = Loop(2, (2, func))
+
+        for O in [one, two, three]:
+            for T in [Loop, Band]:
+                self.assertTrue(isinstance(O, T))
+
+        for O in [one, two, three]:
+            for T in [int, str, list, tuple, dict, Ring]:
+                self.assertFalse(isinstance(O, T))
+
+    def test_equals(self):
+
+        one = Loop(1, 3, 2)
+        two = Loop(1, 3, 2)
+        three = (1, 3, 2)
+        four = Loop(2, 1, 3)
+
+        for O in [one]:
+            for T in [two]:
+                self.assertTrue(O == T)
+
+        for O in [one]:
+            for F in [three, four]:
+                self.assertFalse(O == F)
+
+
+    def test_in(self):
+        loop_true = ['a', 'p']
+        loop_false = ['p', 'a']
+
+        loop_test = Loop('o', 'o', 'a', 'p', 'n')
+
+        self.assertTrue(loop_true in loop_test)
+        self.assertFalse(loop_false in loop_test)
+
+
+class TestRing(unittest.TestCase):
 
     def test_get(self):
         ring = Ring(0, 'hello', alpha)
@@ -40,40 +103,63 @@ class TestRingBase(unittest.TestCase):
         def func():
             a = 2
 
-        ring = Ring(1, 3, 2)
-        ring2 = Ring('1', 3, func)
-        ring3 = Ring(2, (2, func))
+        one = Ring(1, 3, 2)
+        two = Ring('1', 3, func)
+        three = Ring(2, (2, func))
 
-        self.assertTrue(isinstance(ring, Ring))
-        self.assertTrue(isinstance(ring2, Ring))
-        self.assertTrue(isinstance(ring3, Ring))
+        for O in [one, two, three]:
+            for T in [Ring, Band]:
+                self.assertTrue(isinstance(O, T))
 
-        for r in [ring, ring2, ring3]:
-            for t in [int, str, list, tuple, dict]:
-                self.assertFalse(isinstance(r, t))
+        for O in [one, two, three]:
+            for T in [int, str, list, tuple, dict, Loop]:
+                self.assertFalse(isinstance(O, T))
 
     def test_equals(self):
-        # TODO add test for __eq__
-        pass
+
+        one = Ring(1, 3, 2)
+        two = Ring(1, 3, 2)
+        three = (1, 3, 2)
+        four = Ring(2, 1, 3)
+
+        for O in [one]:
+            for T in [four, two]:
+                self.assertTrue(O == T)
+
+        for O in [one]:
+            for F in [three]:
+                self.assertFalse(O == F)
+
+
+class TestContains(unittest.TestCase):
 
     def test_contains(self):
 
-        def func():
-            a = 1
-        def notfunc():
-            b = 2
+        def true_in(I, O):
+            self.assertTrue(I in O, '{} is not in {}'.format(I, O))
 
-        ring = Ring(1, 3, 2)
-        ring2 = Ring('1', 3, func)
-        ring3 = Ring(2, (2, func))
+        def false_in(I, O):
+            self.assertFalse(I in O, '{} is in {}'.format(I, O))
 
-        self.assertTrue(1 in ring)
-        self.assertTrue('1' in ring2)
-        self.assertTrue(((2, func),) in ring3)
+        ap = 'a', 'p'
+        pa = 'p', 'a'
+        no = 'n', 'o'
+        ring_test = Ring('o', 'o', 'a', 'p', 'n')
+        loop_test = Loop('o', 'o', 'a', 'p', 'n')
 
-        for r in [ring, ring2, ring3]:
-            for t in ['s', notfunc, 4]:
-                self.assertFalse(t in r, str(t) +' is in ' +str(r))
+        true_in(ap, ring_test)
+        true_in(Loop(*ap), ring_test)
+        true_in(Ring(*ap), ring_test)
+
+        false_in(pa, ring_test)
+        true_in(Ring(*pa), ring_test)
+        false_in(Loop(*pa), ring_test)
+        true_in(no, ring_test)
+
+        true_in(ap, loop_test)
+        false_in(pa, loop_test)
+        true_in(Ring(*pa), loop_test)
+        false_in(no, loop_test)
 
 
 if __name__ == '__main__':
